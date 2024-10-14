60e368fcef3fac0442be7018fc17b6a4ec06a797
yanuix <ayaanmalik26547@gmail.com>
2024-10-14 23:27:26 +0400

    Add files via upload

---
+++ Print statements.py
@@ -0,0 +1,71 @@
+#1 Basic String Output
+print("Hello World")
+
+#2 Simple Math Inside Print
+A=6
+B=7
+C= A+B
+print(C)
+
+#3 Multiple Arguments in Print
+Word1= "I"
+Word2= "Love"
+Word3= "Python"
+print(Word1+" "+Word2+" "+Word3)
+
+#4 Print with Escape Characters
+print("He Said, \"Hello!\"")
+
+#5 Newline Character
+print("Ayaan Malik\nAamir Malik")
+
+#6 Tab Character
+print("Name   \t Place \t    Animal")
+print("Max \t  Mall   \t Mosquito")
+
+#7 Print Multiple Data Types Together
+text = "The Result is "
+int = 42
+print(text, int)
+
+#8 Print a Single Number
+print(1,2,3,4,5)
+
+#9 Print a Decimal Number
+print(3.14159)
+
+#10 Print Special Characters
+print("Dollar sign: $")
+
+#11 Using the sep Parameter
+print("Car", "Fuel", "Driver", sep=", ")
+
+#12 Using the end Parameter
+print("Hello,", end=" ")
+print("World!")
+
+#13 Print a Repeated String
+print("Python! " * 3)
+
+#14 Print a Unicode Character
+print("\u2764") # Prints a heart symbol
+
+#15 Print Empty Line
+print()
+
+#16 Concatenating Strings Inside Print
+print("Hello " + "World!")
+
+#17 Print Boolean Values
+print(True)
+print(False)
+
+#18 Printing Exponential Notation
+print(1.2e3) # Prints 1200.0
+
+#19 Print Multiline Text Using Triple Quotes
+print("""This is a multiline
+print statement.""")
+
+#20 Print Using Escaped Backslash
+print("This is a backslash: \\")
